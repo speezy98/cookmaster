37e21c38075a45ddd2c65cb5b9e42b593158ebae
uvsq22008841 <lauren.edoh-coffi@ens.uvsq.fr>
2023-11-12 23:41:08 +0100

    Ajout d'une fonction notification
    L'utilisateur va recevoir une notification

---
+++ public/assets/js/notification.js
@@ -0,0 +1,10 @@
+// Définir les mots à concaténer
+var mot1 = "une ";
+var mot2 = "notification";
+
+// Concaténer les mots
+var resultat = mot1 + mot2;
+
+// Afficher le résultat dans la console
+console.log(resultat);
+
