bad4c08fa55396c432dd979c492898f46b88e6a2
uvsq22008841 <lauren.edoh-coffi@ens.uvsq.fr>
2023-11-13 00:13:09 +0100

    Notification
    Les utlisateurs vont desormais recevoir une notification.

---
+++ public/assets/js/mail.js
@@ -0,0 +1,10 @@
+// Définir les mots à concaténer
+var mot1 = "le";
+var mot2 = "mail";
+
+// Concaténer les mots
+var resultat = mot1 + mot2;
+
+// Afficher le résultat dans la console
+console.log(resultat);
+
