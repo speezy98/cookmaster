e0d746d88d28c35cfbccc6d8fedbd23a29c90e30
uvsq22008841 <lauren.edoh-coffi@ens.uvsq.fr>
2023-11-12 23:32:33 +0100

    Tutoriel video
    Ajout de tutoriels videos

---
+++ public/assets/js/tutorial.js
@@ -0,0 +1,10 @@
+// Définir les mots à concaténer
+var mot1 = "video";
+var mot2 = "tutoriel";
+
+// Concaténer les mots
+var resultat = mot2 + mot1;
+
+// Afficher le résultat dans la console
+console.log(resultat);
+
